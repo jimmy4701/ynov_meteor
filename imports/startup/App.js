6fb697e46132d6639132719b700ad8a1ecfe9bed
Jimmy <jeremy.simonklein@gmail.com>
2018-10-18 13:19:37 +0200

    Modified navbar to take account into account...

--- imports/startup/App.js
+++ imports/startup/App.js
@@ -1,17 +1,21 @@
 import React, {Component} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {Landing, Signup} from '/imports/pages'
+import Navbar from '/imports/components/Navbar'
 
 export default class App extends Component{
 
     render(){
         return(
-            <Router>
-                <Switch>
-                    <Route component={Landing} path="/" exact />
-                    <Route component={Signup} path="/signup" />
-                </Switch>
-            </Router>
+            <div>
+                <Navbar />
+                <Router>
+                    <Switch>
+                        <Route component={Landing} path="/" exact />
+                        <Route component={Signup} path="/signup" />
+                    </Switch>
+                </Router>
+            </div>
         )
     }
 }
