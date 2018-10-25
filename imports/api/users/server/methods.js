f3836f7f5d59a20f4066471ad7a2f6e2fd1ac06b
Jimmy <jeremy.simonklein@gmail.com>
2018-10-25 12:00:50 +0200

    Added users.remove

--- imports/api/users/server/methods.js
+++ imports/api/users/server/methods.js
@@ -3,5 +3,8 @@ import {Meteor} from 'meteor/meteor'
 Meteor.methods({
     'users.signup'({email, password}){
         Accounts.createUser({email, password})
+    },
+    'users.remove'(user_id){
+        Meteor.users.remove({_id: user_id})
     }
 })
