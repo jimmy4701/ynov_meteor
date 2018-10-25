6a2665a9a1c9bf669fd50bdf2ac54b0f596f00d6
Jimmy <jeremy.simonklein@gmail.com>
2018-10-25 10:42:01 +0200

    Modified call of Accounts.createUser

--- imports/pages/users/Signup.js
+++ imports/pages/users/Signup.js
@@ -7,12 +7,12 @@ export default class Signup extends Component {
 
     handleChange = (e) => this.setState({[e.target.name]: e.target.value})
 
-    signup = (e) => {
+    submitForm = (e) => {
         e.preventDefault()
         const {email, password} = this.state
-        Meteor.call('users.signup', {email, password}, (error, result) => {
+        Accounts.createUser({email, password}, (error, result) => {
             if(error){
-                alert(error)
+                alert('Erreur inscription', error)
             }else{
                 Meteor.loginWithPassword(email, password)
             }
@@ -25,7 +25,7 @@ export default class Signup extends Component {
         return(
             <div>
                 <p>Inscrivez-vous</p>
-                <form onSubmit={this.signup}>
+                <form onSubmit={this.submitForm}>
                     <input value={email} type="email" name="email" onChange={this.handleChange} placeholder="Email"/>
                     <input value={password} type="password" name="password" onChange={this.handleChange} placeholder="Password"/>
                     <button type="submit">M'inscrire</button>
